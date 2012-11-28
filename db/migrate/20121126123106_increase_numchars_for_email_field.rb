6e8fc09d1490fec96b729dfff3dfaf68f243d845
Jayasainath <jayasainaths@gmail.com>
2012-11-28 17:26:04 +0530

    changed 20121126123106_increase_numchars_for_email_field

--- db/migrate/20121126123106_increase_numchars_for_email_field.rb
+++ db/migrate/20121126123106_increase_numchars_for_email_field.rb
@@ -1,11 +1,11 @@
 class IncreaseNumcharsForEmailField < ActiveRecord::Migration
   
   def up
-  	change_table :posts, :message, :text, :limit => 1000
+  	# change_table :posts, :message, :text, :limit => 1000
   end
 
   def down
-  	change_table :posts, :message, :string, :limit => 150
+  	# change_table :posts, :message, :string, :limit => 150
   end
 
 end
